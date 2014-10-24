ab64d4fe2777a27e9a8de692a7a1adb77581b82d
chloef17 <chloe_fink@asl.org>
2014-10-24 09:14:25 +0100

    Completed task
    Whooh its done!

--- Lab01.java
+++ Lab01.java
@@ -2,8 +2,8 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author Chloe
+* @version 24/10/14
 */
 
 import edu.fcps.karel2.Display;
@@ -12,6 +12,79 @@ import edu.fcps.karel2.Robot;
 public class Lab01 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+    Display.openWorld("maps/school.map");
+   Display.setSize(10, 10);
+   Display.setSpeed(3);
+   Robot pete = new Robot(4,5,Display.SOUTH,0);
+   Robot lisa = new Robot();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.pickBeeper();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.putBeeper();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   
+   pete.move();
+   pete.pickBeeper();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.putBeeper();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+
+
      }
-}
+   }
